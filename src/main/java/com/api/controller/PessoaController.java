121a92f1f8b069be83e5aee6c4e8c61926a8390f
LukeMocroski <devmocroski@gmail.com>
2023-01-22 17:09:39 -0300

    Added Generic methods

--- src/main/java/com/api/controller/PessoaController.java
+++ src/main/java/com/api/controller/PessoaController.java
@@ -3,9 +3,11 @@ package com.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,14 +30,36 @@ public class PessoaController {
 		return pessoaRepository.findAll();
 	}
 	
-	/*
-	 * @GetMapping("/api/{id}") public Optional<Pessoa> selecionaPorId(@PathVariable
-	 * Long id) { return pessoaRepository.findById(id); }
-	 */
-	
+	 
 	@GetMapping("/api/{name}")
 	public List<Pessoa> selecionaPorNome(@PathVariable String name) {
 		return pessoaRepository.findByName(name);
+	}
+	
+	@PutMapping("/api/")
+	public Pessoa editar(@RequestBody Pessoa pessoaObj) {
+		return pessoaRepository.save(pessoaObj);
+		
+	}
+	
+	@DeleteMapping("/api/{id}")
+	public void deletar(@PathVariable Long id) {
+		pessoaRepository.deleteById(id);
+	}
+	
+	@GetMapping("/api/contador")
+	public Long contador() {
+		return pessoaRepository.count();
+	}
+	
+	@GetMapping("/api/ordenarNomes")
+	public List<Pessoa> ordenarNomes(){
+		return pessoaRepository.findByOrderByName();
+	}
+	
+	@GetMapping("/api/nomeContain")
+	public List<Pessoa> nomeContain(){
+		return pessoaRepository.findByNameContaining("t");
 	}
 	
 	
--- src/main/java/com/api/repository/PessoaRepository.java
+++ src/main/java/com/api/repository/PessoaRepository.java
@@ -2,6 +2,7 @@ package com.api.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +15,23 @@ public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 	List<Pessoa> findAll();
 	
 	List<Pessoa> findByName(String name);
+	
+	List<Pessoa> findByOrderByName();
+	
+	/* List<Pessoa> findByNameOrderByParametro(TipoParametro parametro); */
+	
+	List<Pessoa> findByNameContaining(String termo);
+	
+	List<Pessoa> findByNameStartsWith(String termo);
+	
+	List<Pessoa> findByNameEndsWith(String termo);
+	
+	/*
+	 * @Query(value = "SELET SUM(parametro) FROM pessoas", nativeQuery = true) int
+	 * somaId();
+	 */
+	
+	
+	@Query(value = "SELECT * FROM pessoas WHERE parametro >= :parametro", nativeQuery = true)
+	List<Pessoa> parametroMaiorIgual(int parametro);
 }
